27fbfa142c2c4d12d8486aa4ebd83328f8c5f9af
Shubham <Shubham@DESKTOP-OFJPRE7>
2024-03-27 21:54:19 +1100

    Add project files.

---
+++ inventory/urls.py
@@ -0,0 +1,17 @@
+from django.urls import re_path as url
+from .views import *
+
+urlpatterns = [
+
+   url(r'^$', index, name='index'),
+   
+   url(r'^display_laptops$', display_laptops, name='display_laptops'),
+   url(r'^display_desktops$', display_desktops, name='display_desktops'),
+   url(r'^display_mobiles$', display_mobiles, name='display_mobiles'),
+
+   url(r'^add_laptop$', add_laptop, name='add_laptop'),
+   url(r'^add_desktop$', add_desktop, name='add_desktop'),
+   url(r'^add_mobile$', add_mobile, name='add_mobile'),
+
+   
+]
